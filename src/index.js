1a42c130606a8830de050c6e7f8a5b1b96dd2aa3
MHDFahz <fahis.skazi@gmail.com>
2023-02-02 19:53:28 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import App from "./app.js";
 
 const app = new App();
 
+// Lamda Function
 export const handler = async (event, context) => {
   const result = await app.handler(event, context);
   return result;
