f7e81f9901a21a7122f3e8ab5afd7b272fffc15f
MHDFahz <fahis.skazi@gmail.com>
2023-02-02 15:56:55 +0530

    Koa Contoller Context type assertion Added

--- src/controllers/common/index.js
+++ src/controllers/common/index.js
@@ -1,15 +1,22 @@
+import { Context } from "koa";
 import UntilService from "../../utils/index.js";
 
 export class CommonService extends UntilService {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.getinital = this.getinital.bind(this);
     this.getPing = this.getPing.bind(this);
   }
+  /**
+   * @param {Context} ctx The Koa Context
+   */
   async getinital(ctx) {
-    ctx.response.body = "ok"
+    ctx.response.body = "ok";
   }
+  /**
+   * @param {Context} ctx The Koa Context
+   */
   async getPing(ctx) {
-    ctx.throw(new this.NotImplementedError("Some Error Occur"))
+    ctx.throw(new this.NotImplementedError("Some Error Occur"));
   }
 }
