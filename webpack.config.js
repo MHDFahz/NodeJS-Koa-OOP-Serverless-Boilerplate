fc04deaf66e36ec87ba94772325f5ccf27e262f5
MHDFahz <fahis.skazi@gmail.com>
2023-02-02 23:42:15 +0530

    Smal changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 
+const env = process.env.NODE_ENV || 'production';
+const mode = env === 'development' ? 'development' : 'production';
+
 module.exports = {
     entry: slsw.lib.entries,
     output: {
@@ -8,7 +11,7 @@ module.exports = {
         filename: '[name].js',
         path: path.join(__dirname, '.webpack'),
     },
-    mode: 'development',
+    mode,
     target: 'node',
     module: {
         rules: [
